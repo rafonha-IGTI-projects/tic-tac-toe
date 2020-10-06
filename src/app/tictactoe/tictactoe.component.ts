9c048c784cd49ac4b2d75e2a499fd6f17e6251c2
rafonha <johndoe@example.comrafaela.sdp25@gmail.com>
2020-10-06 11:28:26 -0300

    create tictactoe basic structure

---
+++ src/app/tictactoe/tictactoe.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tictactoe',
+  templateUrl: './tictactoe.component.html',
+  styleUrls: ['./tictactoe.component.css']
+})
+export class TictactoeComponent {
+
+}
