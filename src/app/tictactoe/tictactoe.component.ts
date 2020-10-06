0505d117d52d1a95bbb5856c0422c777688f92d6
rafonha <johndoe@example.comrafaela.sdp25@gmail.com>
2020-10-06 12:27:38 -0300

    Determine winner

--- src/app/tictactoe/tictactoe.component.ts
+++ src/app/tictactoe/tictactoe.component.ts
@@ -19,12 +19,40 @@ export class TictactoeComponent {
     if(this.board[row][col] == '' && this.winner == '') {
       this.board[row][col] = this.currentPlayer;
 
+      if(this.checkWinner(this.currentPlayer)) {
+        this.winner = this.currentPlayer;
+      }
+
       if (this.currentPlayer =='O') {
         this.currentPlayer = 'X';
       } else {
         this.currentPlayer = 'O';
       }
     }
+  }
+
+  checkWinner(player: string): boolean {
+    for (let i = 0; i < this.board.length; i++) {
+      if (this.board[i][0] == player && this.board[i][1] == player && this.board[i][2] == player) {
+        return true;
+      }
+    }
+
+    for (let i = 0; i < this.board.length; i++) {
+      if (this.board[0][i] == player && this.board[1][i] == player && this.board[2][i] == player) {
+        return true;
+      }
+    }
+
+    if (this.board[0][0] == player && this.board[1][1] == player && this.board[2][2] == player) {
+      return true;
+    }
+
+    if (this.board[0][2] == player && this.board[1][1] == player && this.board[2][0] == player) {
+      return true;
+    }
+
+    return false;
   }
 
   reset() {
